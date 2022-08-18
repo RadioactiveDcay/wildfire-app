69a9b1cbfcc4aabe2357878a4e66ae8c0162e3f4
RadioactiveDcay <33047670+RadioactiveDcay@users.noreply.github.com>
2022-08-18 16:51:22 -0700

    update: Got the degrees to post finally. It is reading what the API is sendning but the information is not in lame terminology. It reads the temperature as kelvins I think. I am not sure how to convert this yet.

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,32 +1,34 @@
 var city;
 
 window.addEventListener('load', ()=> {
-    let long;
-    let lat;
-    let temperatureDescriptor = document.querySelector('.temperature-descriptor');
-    let temperatureDegree = document.querySelector('.temperature-degree');
-    let locationTimezone = document.querySelector('locationTimezone');
+    // let long;
+    // let lat;
+    // Not sure if these acutally will prove useful. I am unable to find a workaround to pull local Lat Long information
+    let temperatureDescriptor = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.degree');
+    let locationTimezone = document.querySelector('.location-timezone');
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => { 
             const proxy = "https://cors-anywhere.herokuapp.com/";
-            const queryURL = "https://api.openweathermap.org/data/1.0/weather?q=Sacramento&appid=5b2e2bc67a6c73ff4f0998fcebd047ad";
+            const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Sacramento&appid=5b2e2bc67a6c73ff4f0998fcebd047ad";
             // for 'queryURL' Add " &limit=10 " in between Sacramento and &appid
             // This is to limit the amount of times the API is fetched. Openweathermap.org allows for 1k total per day,
             // so to no go above this limit, adding a 'limiter' will reduce that chance.
             const APIkey = `5b2e2bc67a6c73ff4f0998fcebd047ad`;
             
-
+            // This fetch command pulls from the queryURL for the weahter API
         fetch(queryURL)
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                console.log(data);
-                const {weather, temp} = data.main;
-
-                temperatureDegree.textContent = main;
-                
+                // console.log(data);
+                const {temp, weather} = data.main;
+                // Set DOM elements pulled from the API
+                temperatureDegree.textContent = temp;
+                temperatureDescription.textContent = feels_like;
+                locationTimezone.textContent = data.main;
             });
         });
     }
