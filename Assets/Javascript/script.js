84a27283d892d9e56f0d8868881a8b6c456b34b9
RadioactiveDcay <33047670+RadioactiveDcay@users.noreply.github.com>
2022-08-18 16:33:18 -0700

    Update: re-arranged pretty much all of the class / id's around so they make more sense and locating them is not as hard. Reason - this made it easier for me to related the id's to the JS as well as for making the API more relateable on the front page.

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -5,13 +5,15 @@ window.addEventListener('load', ()=> {
     let lat;
     let temperatureDescriptor = document.querySelector('.temperature-descriptor');
     let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('locationTimezone');
 
     if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude; 
+        navigator.geolocation.getCurrentPosition(position => { 
             const proxy = "https://cors-anywhere.herokuapp.com/";
-            const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Sacramento&appid=5b2e2bc67a6c73ff4f0998fcebd047ad";
+            const queryURL = "https://api.openweathermap.org/data/1.0/weather?q=Sacramento&appid=5b2e2bc67a6c73ff4f0998fcebd047ad";
+            // for 'queryURL' Add " &limit=10 " in between Sacramento and &appid
+            // This is to limit the amount of times the API is fetched. Openweathermap.org allows for 1k total per day,
+            // so to no go above this limit, adding a 'limiter' will reduce that chance.
             const APIkey = `5b2e2bc67a6c73ff4f0998fcebd047ad`;
             
 
@@ -21,7 +23,9 @@ window.addEventListener('load', ()=> {
             })
             .then(data => {
                 console.log(data);
-                const {temperature, summary} = data.currently;
+                const {weather, temp} = data.main;
+
+                temperatureDegree.textContent = main;
                 
             });
         });
