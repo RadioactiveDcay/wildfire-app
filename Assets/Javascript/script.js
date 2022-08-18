1a83d026d9cfda581773a9e074813f574d943c7a
RadioactiveDcay <33047670+RadioactiveDcay@users.noreply.github.com>
2022-08-18 14:17:18 -0700

    update: Finally worked out the api key a little more and got it to read the coordinates of Sacramento on the Proxy website console in dev mode. It will actually present itself now and show local information from the provided information. Next I need to make the application populate on the website and show real time data.

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,3 +1,5 @@
+var city;
+
 window.addEventListener('load', ()=> {
     let long;
     let lat;
@@ -8,10 +10,13 @@ window.addEventListener('load', ()=> {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude; 
-            const api = `https://home.openweathermap.org/api_keys/5b2e2bc67a6c73ff4f0998fcebd047ad/${lat},${long}`;
-            const proxy = "https://cors-anywhere.herokuapp.com/"
-        fetch(api)
-            .then(respons => {
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Sacramento&appid=5b2e2bc67a6c73ff4f0998fcebd047ad";
+            const APIkey = `5b2e2bc67a6c73ff4f0998fcebd047ad`;
+            
+
+        fetch(queryURL)
+            .then(response => {
                 return response.json();
             })
             .then(data => {
